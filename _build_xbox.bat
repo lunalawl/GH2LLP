4cf7a2147f7bf823c3eadda2abc3e945553a69f7
Luna <luna.abarta@gmail.com>
2023-01-14 19:14:10 -0800

    Update build scripts

--- _build_xbox.bat
+++ _build_xbox.bat
@@ -1,2 +1,3 @@
+git pull https://github.com/lunalawl/GH2LLP main
 "%~dp0\dependencies\arkhelper.exe" dir2ark "%~dp0\_ark" "%~dp0\_build\gen" -e -s 4073741823
 pause
--- _build_xenia.bat
+++ _build_xenia.bat
@@ -1,3 +1,4 @@
+git pull https://github.com/lunalawl/GH2LLP main
 "%~dp0\dependencies\arkhelper.exe" dir2ark "%~dp0\_ark" "%~dp0\_build\gen" -e -s 4073741823
 "%~dp0\_xenia\xenia_canary" "%~dp0\_build\default.xex"
 pause
